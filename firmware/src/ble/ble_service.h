3397ca7a661a6ce8e05f473596e78b014b62a060
Jason Zhou <zhiye1@hotmail.com>
2021-07-12 00:30:24 +0800

    init

---
+++ firmware/src/ble/ble_service.h
@@ -0,0 +1,26 @@
+
+
+#ifndef BLE_SERVICE_H
+#define BLE_SERVICE_H
+#include <stdbool.h>
+
+    void log_init(void);
+    void timers_init(void);
+    void buttons_leds_init(bool * p_erase_bonds);
+    void power_management_init(void);
+    void ble_stack_init(void);
+    void scheduler_init(void);
+    void gap_params_init(void);
+    void gatt_init(void);
+    void advertising_init(void);
+    void services_init(void);
+    void sensor_simulator_init(void);
+    void conn_params_init(void);
+    void buffer_init(void);
+    void peer_manager_init(void);
+
+    // Start execution.
+    void timers_start(void);
+    void advertising_start(bool erase_bonds);
+
+#endif
---
+++ firmware/src/log/nrf_print.h
@@ -0,0 +1,5 @@
+
+#include "nrf_log.h"
+
+#define nrf_print(word) NRF_LOG_INFO(word)
+
