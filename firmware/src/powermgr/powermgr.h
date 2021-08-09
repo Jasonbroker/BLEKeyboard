76a65fb43746c650719d714f99082f4b3ec36167
Jason Zhou <zhiye1@hotmail.com>
2021-08-10 01:34:32 +0800

    feat add pwrmgr

---
+++ firmware/src/powermgr/powermgr.h
@@ -0,0 +1,4 @@
+
+void kb_power_mgr_init();
+
+void kb_power_mgr_start();
--- firmware/src/protocol/keymap_common.h
+++ firmware/src/protocol/keymap_common.h
@@ -33,19 +33,19 @@ extern const action_t fn_actions[];
  * K2C, K31 and  K3C are extra keys for ISO
  */
 #define KEYMAP( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, \
-	K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D, \
-	K50, K51, K52,           K55,           K58, K59, K5A, K5B, K5C, K5D \
+	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0F,\
+	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1F,\
+	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2F,\
+	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3F,\
+	K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D, K4F,\
+	K50, K51, K52,           K55,      K57, K58, K59, K5A, K5B, K5C, K5D, K5F\
 ) { \
-	{ K00,    K01,    K02,    K03,    K04,    K05,    K06,    K07,    K08,    K09,    K0A,    K0B,    K0C,    K0D }, \
-	{ K10,    K11,    K12,    K13,    K14,    K15,    K16,    K17,    K18,    K19,    K1A,    K1B,    K1C,    K1D }, \
-	{ K20,    K21,    K22,    K23,    K24,    K25,    K26,    K27,    K28,    K29,    K2A,    K2B,    K2C,    K2D }, \
-	{ K30,    K31,    K32,    K33,    K34,    K35,    K36,    K37,    K38,    K39,    K3A,    K3B,    K3C,    K3D }, \
-	{ K40,    K41,    K42,    K43,    K44,    K45,    K46,    K47,    K48,    K49,    K4A,    K4B,    K4C,    K4D }, \
-	{ K50,    K51,    K52,    KC_NO,  KC_NO,  K55,    KC_NO,  KC_NO,  K58,    K59,    K5A,    K5B,    K5C,    K5D }  \
+	{ K00,    K01,    K02,    K03,    K04,    K05,    K06,    K07,    K08,    K09,    K0A,    K0B,    K0C,    K0D , K0F}, \
+	{ K10,    K11,    K12,    K13,    K14,    K15,    K16,    K17,    K18,    K19,    K1A,    K1B,    K1C,    K1D ,K1F}, \
+	{ K20,    K21,    K22,    K23,    K24,    K25,    K26,    K27,    K28,    K29,    K2A,    K2B,    K2C,    K2D ,K2F}, \
+	{ K30,    K31,    K32,    K33,    K34,    K35,    K36,    K37,    K38,    K39,    K3A,    K3B,    K3C,    K3D ,K3F}, \
+	{ K40,    K41,    K42,    K43,    K44,    K45,    K46,    K47,    K48,    K49,    K4A,    K4B,    K4C,    K4D ,K4F}, \
+	{ K50,    K51,    K52,    KC_NO,  KC_NO,  K55,    KC_NO,  KC_NO,  K58,    K59,    K5A,    K5B,    K5C,    K5D ,K5F}  \
 }
 
 
